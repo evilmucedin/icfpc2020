fc9ef769fff0ffdbd444dae7137c8681e1ca30cf
U-REDMOND\deraskov <deraskov@denplusplus.redmond.corp.microsoft.com>
2020-07-15 12:17:58 -0700

    fix

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@ mkdir -p build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make -j 8
-rm -f ../release/build/app.exe
-cp ../release/build/app .
-cp ../release/build/app.exe app
+rm -f ../release/build/app.exe || true
+cp ../release/build/app . || true
+cp ../release/build/app.exe app || true
 # cd src/app
 # mkdir -p ../../build
 # g++ -std=c++11 -o ../../build/main main.cpp
