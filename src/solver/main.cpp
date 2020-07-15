68535d8e7e579da1da107cd451a8fee029ae5e03
Loks- <alexeypoyarkov@gmail.com>
2020-07-14 21:53:56 -0700

    Replace include.

--- src/solver/main.cpp
+++ src/solver/main.cpp
@@ -1,14 +1,13 @@
 #include "common/numeric/long/multiplication.h"
 #include "common/numeric/long/unsigned_io.h"
 #include "common/numeric/utils/pow.h"
-
-#include <iostream>
+#include "common/stl/base.h"
 
 using namespace std;
 
 int main() {
   LongUnsigned two(2u);
-  std::cout << PowU(two, 200u) << endl;
-  std::cout << PowU(two, 70u) * PowU(two, 130u) << endl;
+  cout << PowU(two, 200u) << endl;
+  cout << PowU(two, 70u) * PowU(two, 130u) << endl;
   return 0;
 }
