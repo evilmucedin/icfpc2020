dcc896b13c04b637154e1eb57a2297bde6e49aa1
Loks- <alexeypoyarkov@gmail.com>
2020-07-14 21:38:08 -0700

    Add vs code files for auto keeping style.

--- src/solver/main.cpp
+++ src/solver/main.cpp
@@ -1,5 +1,5 @@
-#include "common/numeric/long/unsigned_io.h"
 #include "common/numeric/long/multiplication.h"
+#include "common/numeric/long/unsigned_io.h"
 #include "common/numeric/utils/pow.h"
 
 #include <iostream>
@@ -7,8 +7,8 @@
 using namespace std;
 
 int main() {
-    LongUnsigned two(2u);
-    std::cout << PowU(two, 200u) << endl;
-    std::cout << PowU(two, 70u) * PowU(two, 130u) << endl;
-    return 0;
+  LongUnsigned two(2u);
+  std::cout << PowU(two, 200u) << endl;
+  std::cout << PowU(two, 70u) * PowU(two, 130u) << endl;
+  return 0;
 }
