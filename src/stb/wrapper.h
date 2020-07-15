ca87f6a6c602260a20d29d5b24d8da0f6b14a0f0
U-REDMOND\deraskov <deraskov@denplusplus.redmond.corp.microsoft.com>
2020-07-15 14:44:03 -0700

    beef PNGWrapper API; add sample

--- src/stb/wrapper.h
+++ src/stb/wrapper.h
@@ -1,14 +1,20 @@
 #pragma once
 
-#include <string>
 #include <memory>
+#include <string>
 #include <vector>
 
 class PNGWrapper {
-  public:
-    PNGWrapper(const std::string& filename);
+ public:
+  PNGWrapper(const std::string& filename);
+  ~PNGWrapper();
 
-  private:
-    class Impl;
-    std::unique_ptr<Impl> impl_;
+  int getWidth() const;
+  int getHeight() const;
+  int getChannels() const;
+  unsigned char get(size_t channel, size_t x, size_t y) const;
+
+ private:
+  class Impl;
+  std::unique_ptr<Impl> impl_;
 };
