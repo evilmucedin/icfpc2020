6ac8a30a851dfcf09529dd48e986bfb350b524e7
U-REDMOND\deraskov <deraskov@denplusplus.redmond.corp.microsoft.com>
2020-07-15 15:06:54 -0700

    beef the app

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd build
-exec ./app "$@"
+./app "$@" || echo "run error code: $?"
--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,27 +1,41 @@
-#include <iostream>
 #include "httplib.h"
+#include <iostream>
+#include <regex>
+#include <string>
 
-int main(int , char* argv[])
-{
-    std::string server_url(argv[1]);
-    std::string player_key(argv[2]);
+int main(int /*argc*/, char* argv[]) {
+  const std::string serverUrl(argv[1]);
+  const std::string playerKey(argv[2]);
 
-    std::cout << "ServerUrl: " << server_url << "; PlayerKey: " << player_key << std::endl;
+  std::cout << "ServerUrl: " << serverUrl << "; PlayerKey: " << playerKey
+            << std::endl;
 
-    auto url = server_url.substr(server_url.find(":") + 3); // Remove schema
-    auto colonPosition = url.find(":");
-    auto pathPosition = url.find("/") != std::string::npos ? url.find("/") : url.length();
-    auto host = url.substr(0, colonPosition);
-    auto port = std::stoi(url.substr(colonPosition + 1, pathPosition));
-    auto path = url.substr(pathPosition);
+  const std::regex urlRegexp("http://(.+):(\\d+)");
+  std::smatch urlMatches;
+  if (!std::regex_search(serverUrl, urlMatches, urlRegexp) ||
+      urlMatches.size() != 3) {
+    std::cout << "Unexpected server response:\nBad server URL" << std::endl;
+    return 1;
+  }
+  const std::string serverName = urlMatches[1];
+  const int serverPort = std::stoi(urlMatches[2]);
+  httplib::Client client(serverName, serverPort);
+  const std::shared_ptr<httplib::Response> serverResponse =
+      client.Post(serverUrl.c_str(), playerKey.c_str(), "text/plain");
 
-    httplib::Client cli(host, port);
+  if (!serverResponse) {
+    std::cout << "Unexpected server response:\nNo response from server"
+              << std::endl;
+    return 1;
+  }
 
-    auto res = cli.Get((path + "?playerKey=" + player_key).c_str());
+  if (serverResponse->status != 200) {
+    std::cout << "Unexpected server response:\nHTTP code: "
+              << serverResponse->status
+              << "\nResponse body: " << serverResponse->body << std::endl;
+    return 2;
+  }
 
-    if (!res || res->status != 200) {
-        return 1;
-    }
-
-    return 0;
+  std::cout << "Server response: " << serverResponse->body << std::endl;
+  return 0;
 }
