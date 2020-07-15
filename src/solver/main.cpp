200fb0f6730079d8e4296a0da3507daa80eea192
Loks- <alexeypoyarkov@gmail.com>
2020-07-14 21:37:01 -0700

    Add small main to check that common library working.

---
+++ src/solver/main.cpp
@@ -0,0 +1,14 @@
+#include "common/numeric/long/unsigned_io.h"
+#include "common/numeric/long/multiplication.h"
+#include "common/numeric/utils/pow.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    LongUnsigned two(2u);
+    std::cout << PowU(two, 200u) << endl;
+    std::cout << PowU(two, 70u) * PowU(two, 130u) << endl;
+    return 0;
+}
