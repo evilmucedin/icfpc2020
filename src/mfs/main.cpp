27fa060aa038807d3827c5d34ef05be9352645b8
Loks- <alexeypoyarkov@gmail.com>
2020-07-15 22:01:32 -0700

    Add converter png -> MatrixBool.

--- src/mfs/main.cpp
+++ src/mfs/main.cpp
@@ -1,31 +1,28 @@
 #include "glyph_decoder.h"
+#include "message_as_image.h"
 
 #include "common/stl/base.h"
 
 #include "stb/wrapper.h"
 
+#include <string>
+
+namespace {
+std::string messages_dir = "../src/mfs/messages/";
+}
+
 void PNGExample(const std::string& filename) {
-  PNGWrapper w(filename);
-  std::cout << w.getChannels() << " " << w.getWidth() << " " << w.getHeight()
-            << std::endl;
-  for (size_t i = 0; i < w.getChannels(); ++i) {
-    for (size_t j = 0; j < w.getHeight(); ++j) {
-      for (size_t k = 0; k < w.getWidth(); ++k) {
-        std::cout << unsigned(w.get(i, j, k)) << " ";
-      }
-      std::cout << std::endl;
-    }
-    std::cout << std::endl;
-  }
+  MessageAsImage m(filename);
+  m.Print();
 }
 
 int main() {
-  // PNGExample("../src/mfs/messages/message14.png");
-  GlyphDecoder gd = GlyphDecoder::GetDecoder();
-  for (int64_t i = -10; i < 30; ++i) {
-    auto ei = gd.Encode(Glyph(GlyphType::NUMBER, i));
-    auto di = gd.Decode(ei);
-    cout << i << " -> " << ei.mask << " -> " << di.value << endl;
-  }
+  PNGExample("../src/mfs/messages/message14.png");
+  //   GlyphDecoder gd = GlyphDecoder::GetDecoder();
+  //   for (int64_t i = -10; i < 30; ++i) {
+  //     auto ei = gd.Encode(Glyph(GlyphType::NUMBER, i));
+  //     auto di = gd.Decode(ei);
+  //     cout << i << " -> " << ei.mask << " -> " << di.value << endl;
+  //   }
   return 0;
 }
---
+++ src/mfs/message_as_image.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "common/linear_algebra/bool/matrix.h"
+
+#include <string>
+
+class MessageAsImage {
+ public:
+  const unsigned png_scale = 4;
+
+  la::MatrixBool m;
+
+ public:
+  MessageAsImage();
+  MessageAsImage(const std::string& png_file_name);
+
+  void ReadPNG(const std::string& png_file_name);
+  void Print() const;
+};
