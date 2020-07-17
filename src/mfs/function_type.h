04993a7a9d390198341884033a9de4421714b54b
Loks- <alexeypoyarkov@gmail.com>
2020-07-17 09:12:02 -0700

    Rename ftypes.

--- src/mfs/function_type.h
+++ src/mfs/function_type.h
@@ -16,12 +16,12 @@ enum class FunctionType {
   S_COMBINATOR = 18,
   C_COMBINATOR = 19,
   B_COMBINATOR = 20,
-  K_COMBINATOR = 21,  // Also works as 'True' from boolean operators
-  FALSE_SECOND = 22,  // Also works as 'False' from boolean operators
+  K_COMBINATOR = 21,   // Also works as 'True' from boolean operators
+  FALSE__SECOND = 22,  // Also works as 'False' from boolean operators
   POWER_OF_TWO = 23,
   I_COMBINATOR = 24,
-  CONS_PAIR = 25,
-  CAR_FIRST = 26,
-  CDR_TAIL = 27,
+  CONS__PAIR = 25,
+  CAR__FIRST = 26,
+  CDR__TAIL = 27,
   END
 };
