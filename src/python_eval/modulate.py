bd61d7b21e6e4d32b9ce440cd86329cb8ae754c2
pivizz <pivizz@gmail.com>
2020-07-18 11:11:11 -0700

    add demodulator

--- src/python_eval/modulate.py
+++ src/python_eval/modulate.py
@@ -64,3 +64,78 @@ assert to_str(modulate_anything(-1)) == '10100001'
 assert to_str(modulate_anything((1, 2))) == '110110000101100010'
 assert to_str(modulate_anything([1, (2, 3), 4])) == '110110000111110110001001100011110110010000'
 assert to_str(modulate_anything([1, [2, 3], 4])) == '1101100001111101100010110110001100110110010000'
+
+
+# demodulation input is a string + coordinate to start read from, output is value + position of reading
+def _demodulate_number_no_sign(val, ind):
+    ones = 0
+    while val[ind] == '1':
+        ind += 1
+        ones += 1
+    ind += 1 # skip '0'
+    if ones == 0:
+        return 0, ind
+    end = ind + 4 * ones
+    assert len(val) >= end
+    n = val[ind:end]
+    ind = end
+    return int(n, 2), ind
+
+# demodulation input is a string + coordinate to start read from, output is value + position of reading
+def _demodulate_number(val, ind):
+    start = val[ind:(ind+2)]
+    if start == '01':
+        sgn = 1
+    elif start == '10':
+        sgn = -1
+    else:
+        assert False, "Wrong number format"
+    ind += 2
+    n, ind = _demodulate_number_no_sign(val, ind)
+    return sgn * n, ind
+
+def _demodulate_anything(val, ind):
+    prefix = val[ind:ind+2]
+    assert len(prefix) == 2
+    ind += 2
+    if prefix == '00':
+        return [], ind
+    elif prefix == '11':
+        # Can be list or a pair
+        res = []
+        while True:
+            elem, ind = _demodulate_anything(val, ind)
+            res.append(elem)
+            assert ind+2 <= len(val)
+            p = val[ind: ind+2]
+            ind += 2
+            if p == '00':
+                return res, ind
+            elif p == '11':
+                continue
+            else:
+                # not a list - not ending in a nil
+                ind -= 2 # retract
+                n, ind = _demodulate_number(val, ind)
+                ret = n
+                while len(res) > 0:
+                    ret = (res.pop(), ret)
+                return ret, ind
+    else:
+        ind -= 2
+        return _demodulate_number(val, ind)
+
+def demodulate(val):
+    return _demodulate_anything(val, 0)[0]
+
+
+assert to_str(modulate_anything(demodulate('010'))) == '010'
+assert to_str(modulate_anything(demodulate('01100011'))) == '01100011'
+assert to_str(modulate_anything(demodulate('110110000101100010'))) == '110110000101100010'
+assert to_str(modulate_anything(demodulate('110110000111110110001001100011110110010000'))) == '110110000111110110001001100011110110010000'
+assert to_str(modulate_anything(demodulate('1101100001111101100010110110001100110110010000'))) == '1101100001111101100010110110001100110110010000'
+
+#print(demodulate('110110000111110110001001100011110110010000'))
+#print(demodulate('1101100001111101100010110110001100110110010000'))
+
+
