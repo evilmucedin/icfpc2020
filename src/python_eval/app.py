d175feae41d123600dfab2380da15ca2b105fbef
pivizz <pivizz@gmail.com>
2020-07-19 12:13:28 -0700

    Use same OrbiterStrategy in both app.py and autorunner, combine picking stats and next turn actions in the strategy class

--- src/python_eval/app.py
+++ src/python_eval/app.py
@@ -1,15 +1,16 @@
 import requests
 import sys
 
-from orbiter import orbiter_strategy
+from orbiter import OribterStrategy
 from interaction import send2
 
-def player(key, strategy, server_url):
-    send2([2, key, []], server_url=server_url)
-    state = send2([3, key, [256, 5, 13, 1]], server_url=server_url)
+def player(key, strategy, sender):
+    res = sender([2, key, []])
+    initial_stats = strategy.pick_stats(res)
+    state = sender([3, key, initial_stats])
     images = []
     while True:
-        state = send2([4, key, strategy(state)], server_url=server_url)
+        state = sender([4, key, strategy.apply(state)])
         if state[1] == 2:
             print('done')
             break
@@ -18,8 +19,9 @@ def main():
     server_url = sys.argv[1]
     player_key = sys.argv[2]
     print('ServerUrl: %s; PlayerKey: %s' % (server_url, player_key))
-
-    player(int(player_key), orbiter_strategy, server_url)
+    sender = lambda msg: send2(msg, server_url=server_url)
+    strategy = OrbiterStrategy(shoot=True, printships=False, duplicate=False)
+    player(int(player_key), strategy, sender=sender)
 
 
 if __name__ == '__main__':
